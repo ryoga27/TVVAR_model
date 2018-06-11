8650b22c3c3c20c372009ab5cf8d27820954445a
Ryoga Kobayashi <ryoga.kobayashi@icloud.com>
2018-06-11 22:24:03 +0900

    fix bugs

--- code/predict.R
+++ code/predict.R
@@ -1,7 +1,8 @@
-predict = function(y, result, t){
-    nu = result$nu
+predict = function(y, result, t, p = 1){
+    d = length(result$nu)
+    nu = matrix(result$nu, nrow = d, ncol = 1)
     Phi = result$Phi
-    Zt = make_Zt(y, t, p)
-    out = nu + Phi[, , t]%*%Zt
+    Zt = make_Zt(y, t, p = p)
+    out = nu + as.matrix(Phi[, , t], nrow = d, ncol = d)%*%Zt
     return(c(out))
 }
